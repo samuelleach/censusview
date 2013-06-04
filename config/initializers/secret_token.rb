149ed7453ee3cb68b727ae254f5ed91d0e754f15
Samuel Leach <samuel.leach@gmail.com>
2013-06-04 11:54:53 +0100

    Ran: rails new . --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Censusview::Application.config.secret_token = 'dd6627cc3765b97c0a3db4383327f3ee55277861c0b9f242d95a386a5b708d32e993d2a626f80c43c6f2c5cae0dc1a38beb58d133cb3e062212634ee528ce19b'
