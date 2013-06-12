07795c7a9e89d112b05459bb38e346b296980236
Samuel Leach <samuel.leach@gmail.com>
2013-06-12 13:06:10 +0100

    Adding event handler

--- app/assets/javascripts/census_var_typeahead.js
+++ app/assets/javascripts/census_var_typeahead.js
@@ -5,10 +5,14 @@ $(document).ready(function() {
 	  valueKey: 'varlabel',
 	  limit: 200,
 	  template: [                                                                 
-	    '<a href="{{varlabel}}"><p class="censusvar-label">{{varlabel}}</p>',                              
+	    '<a href="{{varlabel}}">',
+	    '<p class="censusvar-label">{{varlabel}}</p>',                              
 	    '<p class="censusvar-units">{{units}}</p>',                                      
-	    '<p class="censusvar-longlabel">{{longlabel}}</p></a>'                         
+	    '<p class="censusvar-longlabel">{{longlabel}}</p>',
+	    '</a>'                         
 	  ].join(''),                                                                 
 	  engine: Hogan                                                               
+	}).on('typeahead:selected', function(event, data) {
+      	window.location.href='/census_counts/'+data.varlabel;
 	});
 });
