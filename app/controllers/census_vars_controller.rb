832d5032f02df2878f615002c1016ed155e75c2e
Samuel Leach <samuel.leach@gmail.com>
2013-06-12 13:07:55 +0100

    Removing tokens calculation

--- app/controllers/census_vars_controller.rb
+++ app/controllers/census_vars_controller.rb
@@ -5,12 +5,12 @@ class CensusVarsController < ApplicationController
     @census_vars = CensusVar.all
     @longlabels = CensusVar.select(:longlabel)
 
-    # could put this into the model with a callback or seed
-    @census_vars.each_with_index do |census_var, i|
-    	census_var.tokens = census_var.longlabel.to_s.split(' ')
-    	census_var.tokens.append @census_vars[i][:varlabel]
-    	census_var.tokens.append 'all'
-    end
+    # Tokenization. Removing this - should be done automatically by Twitter Typahead
+    # @census_vars.each_with_index do |census_var, i|
+    # 	census_var.tokens = census_var.longlabel.to_s.split(' ')
+    # 	census_var.tokens.append @census_vars[i][:varlabel]
+    # 	census_var.tokens.append 'all'
+    # end
 
     respond_to do |format|
       format.json
